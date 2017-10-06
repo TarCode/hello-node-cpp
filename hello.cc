2f7fe247fcfade34946ec06b2cf2e4037131d066
TarCode <tarcode33@gmail.com>
2017-10-06 15:57:42 +0200

    configured and built hello.cc using node-gyp. Imported build addon into main.js and called hello method

--- hello.cc
+++ hello.cc
@@ -1,14 +1,14 @@
 #include <node.h>
 
 namespace demo {
-    using v8::FunctionalCallbackInfo;
+    using v8::FunctionCallbackInfo;
     using v8::Isolate;
     using v8::Local;
     using v8::Object;
     using v8::String;
     using v8::Value;
 
-    void Method (const FunctionalCallbackInfo<Value> & args) {
+    void Method (const FunctionCallbackInfo<Value> & args) {
         Isolate* isolate = args.GetIsolate();
         args.GetReturnValue().Set(String::NewFromUtf8(isolate, "world"));
     }
